695a2434a80dfba3c2fda5c5b91222945000e1f8
zmmohibul <18-36427-1@student.aiub.edu>
2024-05-13 17:56:46 +0600

    Render site name change

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_13_061146) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
